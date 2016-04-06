c687fe8a67a148e40dc73f7d0e4a1d2c8a95a3f1
Jose Hernandez <josethernandezc@hotmail.com>
2016-04-06 13:10:55 -0700

    Edit and destroy labels

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -1,6 +1,10 @@
 class LabelsController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @labels = Label.all
+  end
+
   def new
     @label = Label.new
   end
@@ -9,10 +13,38 @@ class LabelsController < ApplicationController
     @label = Label.new(secure_params)
 
     if @label.save
+      flash[:notice] = "The label was created."
       redirect_to labels_path
     else
-      render 'new'
+      render "new"
     end
+  end
+
+  def edit
+    @label = Label.find(params[:id])
+  end
+
+  def update
+    @label = Label.find(params[:id])
+
+    if @label.update(secure_params)
+      flash[:notice] = "The label was updated."
+      redirect_to labels_path
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    label = Label.find(params[:id])
+
+    if label.destroy
+      flash[:notice] = "The label was destroyed."
+    else
+      flash[:notice] = "An error occurred, please try again."
+    end
+
+    redirect_to labels_path
   end
 
   private
--- app/models/label.rb
+++ app/models/label.rb
@@ -1,5 +1,5 @@
 class Label < ActiveRecord::Base
   validates :name, :colour, presence: true
   validates :name, uniqueness: { scope: :colour, message: "has already been taken with that colour" }
-  validates :colour, inclusion: { in: COLOURS["standard"], message: "is not valid" }
+  validates :colour, inclusion: { in: COLOURS["standard"], message: "is not valid" }, if: "colour.present?"
 end
